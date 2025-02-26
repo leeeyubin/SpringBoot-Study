192be888d7536ec7c2f8e7b8e0000a2dfd29d7f2
LEE YOU BIN <youbeene3409@naver.com>
2025-02-27 02:58:52 +0900

    feat : make MemberService

--- src/test/java/com/springboot/study/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/springboot/study/repository/MemoryMemberRepositoryTest.java
@@ -11,12 +11,12 @@ class MemoryMemberRepositoryTest {
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
     @AfterEach
-    public void afterEach(){
+    public void afterEach() {
         repository.clearStore();
     }
 
     @Test
-    public void save(){
+    public void save() {
         Member member = new Member();
         member.setName("spring");
 
@@ -27,7 +27,7 @@ class MemoryMemberRepositoryTest {
     }
 
     @Test
-    public void findByName(){
+    public void findByName() {
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
@@ -41,7 +41,7 @@ class MemoryMemberRepositoryTest {
     }
 
     @Test
-    public void findAll(){
+    public void findAll() {
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
